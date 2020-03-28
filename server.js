6208c5569a6d4e9522e83a3919583d482e3bafcb
Chandler Willoughby <chandler243@gmail.com>
2020-03-28 01:59:35 -0400

    Update port for Heroku

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
+let port = process.env.PORT
+if (port == null || port == "") {
+	port = 8080
+}
+
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });

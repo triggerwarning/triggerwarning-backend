b5fa957797ac64aa30d0b137ab0e461c982d1b60
Chandler Willoughby <chandler243@gmail.com>
2020-03-28 04:36:11 -0400

    Update cardset.js

--- routes/cardset.js
+++ routes/cardset.js
@@ -8,11 +8,18 @@ module.exports = function(server) {
 	})
 
 	server.post('/api/cardset', function(req, res, next) {
-		models.CardSet.findOrCreate({ name: req.body.name },{
-				description: req.body.description,
-				vendor: req.body.vendor
-			}).success(function(cardset, created) {
-			res.send(cardset)
+		models.CardSet.find({ where: { name: req.body.name } }).success(function(cardset) {
+			if (cardset) {
+				res.send(400, 'A cardset with this name already exists, please change the name or edit the existing cardset')
+			} else {
+				var cardset = models.CardSet.create({
+					name: req.body.name,
+					description: req.body.description,
+					vendor: req.body.vendor
+				}).then(function(cardset) {
+					res.send(cardset)
+				})
+			}
 		})
 	})
 }

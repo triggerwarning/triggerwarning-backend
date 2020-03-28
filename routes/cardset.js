e723d4d31cfe80d4238bc5570d1a1cb8e627786c
Chandler Willoughby <chandler243@gmail.com>
2020-03-28 15:13:00 -0400

    Update cardset.js

--- routes/cardset.js
+++ routes/cardset.js
@@ -11,7 +11,7 @@ module.exports = function(server) {
 	server.post('/api/cardset', function(req, res, next) {
 		CardSet.findOne({ where: { name: req.body.name } }).then(function(cardset) {
 			if (cardset) {
-				res.send(400, 'A cardset with this name already exists, please change the name or edit the existing cardset')
+				res.send(400, { err:'A cardset with this name already exists, please change the name or edit the existing cardset'})
 			} else {
 				var cardset = models.CardSet.create({
 					name: req.body.name,

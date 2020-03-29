96073c59217bd442946d91a145a351fc29b953a0
Chandler Willoughby <chandler243@gmail.com>
2020-03-29 15:26:08 -0400

    Testing json endpoint

--- routes/promptcard.js
+++ routes/promptcard.js
@@ -9,18 +9,19 @@ module.exports = function(server) {
 	})
 
 	server.post('/api/promptcard', function(req, res, next) {
-		PromptCard.findOne({ where: { text: req.body.text } }).then(function(promptcard) {
-			if (promptcard) {
-				res.send(400, { err:'A prompt card with this text already exists'})
-			} else {
-				var promptcard = models.PromptCard.create({
-					text: req.body.text,
-					draw: req.body.draw,
-					pick: req.body.pick
-				}).then(function(promptcard) {
-					res.send(promptcard)
-				})
-			}
-		})
+		console.log(req)
+		// PromptCard.findOne({ where: { text: req.body.text } }).then(function(promptcard) {
+		// 	if (promptcard) {
+		// 		res.send(400, { err:'A prompt card with this text already exists'})
+		// 	} else {
+		// 		var promptcard = models.PromptCard.create({
+		// 			text: req.body.text,
+		// 			draw: req.body.draw,
+		// 			pick: req.body.pick
+		// 		}).then(function(promptcard) {
+		// 			res.send(promptcard)
+		// 		})
+		// 	}
+		// })
 	})
 }

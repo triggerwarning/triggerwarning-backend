e14f00018be75dcea302bcf14f3067f54cd5aa79
Chandler Willoughby <chandler243@gmail.com>
2020-03-29 15:35:12 -0400

    Update promptcard.js

--- routes/promptcard.js
+++ routes/promptcard.js
@@ -9,19 +9,18 @@ module.exports = function(server) {
 	})
 
 	server.post('/api/promptcard', function(req, res, next) {
-		console.log(req)
-		// PromptCard.findOne({ where: { text: req.body.text } }).then(function(promptcard) {
-		// 	if (promptcard) {
-		// 		res.send(400, { err:'A prompt card with this text already exists'})
-		// 	} else {
-		// 		var promptcard = models.PromptCard.create({
-		// 			text: req.body.text,
-		// 			draw: req.body.draw,
-		// 			pick: req.body.pick
-		// 		}).then(function(promptcard) {
-		// 			res.send(promptcard)
-		// 		})
-		// 	}
-		// })
+		PromptCard.findOne({ where: { text: req.body.text } }).then(function(promptcard) {
+			if (promptcard) {
+				res.send(400, { err:'A prompt card with this text already exists'})
+			} else {
+				var promptcard = models.PromptCard.create({
+					text: req.body.text,
+					draw: req.body.draw,
+					pick: req.body.pick
+				}).then(function(promptcard) {
+					res.send(promptcard)
+				})
+			}
+		})
 	})
 }
